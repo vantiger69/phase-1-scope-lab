98237ad718c859ffbae844bbd63ae9d97d6bda71
Titus kipchirchir <vantiger817@tgmail.com>
2024-07-29 16:17:41 +0300

    complete_lap

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "some initial value";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "new value"; // This will cause an error
+}
